18ab7d9c0f8b721811c5e34c71f307783a68eb9e
firewave <firewave@users.noreply.github.com>
2024-03-05 16:27:40 +0100

    fixture.h: properly handle stray exceptions in `TEST_CASE`

--- test/fixture.h
+++ test/fixture.h
@@ -272,7 +272,7 @@ public:
 };
 
 // TODO: most asserts do not actually assert i.e. do not return
-#define TEST_CASE( NAME )  do { if (prepareTest(#NAME)) { setVerbose(false); NAME(); teardownTest(); } } while (false)
+#define TEST_CASE( NAME )  do { if (prepareTest(#NAME)) { setVerbose(false); try { NAME(); } catch (...) { assertNoThrowFail(__FILE__, __LINE__); } teardownTest(); } } while (false)
 #define ASSERT( CONDITION )  if (!assert_(__FILE__, __LINE__, (CONDITION))) return
 #define ASSERT_LOC( CONDITION, FILE_, LINE_ )  assert_(FILE_, LINE_, (CONDITION))
 #define CHECK_EQUALS( EXPECTED, ACTUAL )  assertEquals(__FILE__, __LINE__, (EXPECTED), (ACTUAL))
